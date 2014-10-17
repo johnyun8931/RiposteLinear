d32b3ac08bbf348cde74aa4deb9f81ed9224bbc1
Henry Corrigan-Gibbs <henry.corrigan-gibbs.remove-this-text@yale.remove-this-text.edu>
2014-10-16 17:56:17 -0700

    Resizing table

--- db/types.go
+++ db/types.go
@@ -16,14 +16,14 @@ const NUM_DIMENSIONS = 2
 const NUM_SERVERS = 2//1 << NUM_DIMENSIONS
 
 // Size of a side of the data array
-const TABLE_WIDTH int = 1 << 8
-const TABLE_HEIGHT int = 1 << 9
+const TABLE_WIDTH int = 1 << 5
+const TABLE_HEIGHT int = 1 << 12
 
 // Number of upload requests to buffer
 const REQ_BUFFER_SIZE int = 8
 
 // Length of plaintext messages (in bytes)
-const SLOT_LENGTH int = 256// 64 KB
+const SLOT_LENGTH int = 160// 64 KB
 
 type BitMatrix [TABLE_HEIGHT]BitMatrixRow
 type BitMatrixRow [TABLE_WIDTH*SLOT_LENGTH]byte
